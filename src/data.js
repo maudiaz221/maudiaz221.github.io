6732798272117cf63c4c65c063b6871f47927de7
Mauricio Diaz <diazmauricio221@gmail.com>
2023-10-09 22:55:49 -0600

    Added personal details to website

--- src/data.js
+++ src/data.js
@@ -5,7 +5,9 @@ import { Icon } from "@iconify/react";
 // import newLogo from "./images/yourFileName"
 
 // Projects Images (add your images to the images directory and import below)
-import Logo from "./images/logo.svg";
+import Logo from "./images/ship.bmp";
+import Logo1 from "./images/domino.png";
+import Logo3 from "./images/google_reviews.jpeg";
 
 // Hero Images (add your images to the /images directory with the same names)
 import HeroLight from "./images/hero-light.jpg";
@@ -18,21 +20,22 @@ export { HeroDark as Dark };
  **************************************************************
   Add your GitHub username (string - "YourUsername") below.
 */
-export const githubUsername = "Your GitHub username here";
+export const githubUsername = "maudiaz221";
 
 // Navbar Logo image
 export const navLogo = undefined;
 
 // Blog link icon - https://icon-sets.iconify.design/
-export const Blog = <Icon icon="ph:link-bold" />;
+export const Blog = <Icon icon="mdi:instagram" />;
+
+
 
 /* About Me
  **************************************************************
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I built this site with React, React Bootstrap, Redux, and the GitHub REST API.";
-
+      "I have a passion for coding and maths, I love spending time with my friends and family, and working out. I love challenging myself and always pushing my boundaries. "
 /* Skills
  ************************************************************** 
   Add or remove skills in the SAME format below, choose icons here - https://icon-sets.iconify.design/
@@ -40,38 +43,38 @@ export const moreInfo =
 export const skillData = [
   {
     id: 1,
-    skill: <Icon icon="mdi:language-html5" className="display-4" />,
-    name: "HTML5",
+    skill: <Icon icon="teenyicons:python-outline" className="display-4" />,
+    name: "Python",
   },
   {
     id: 2,
-    skill: <Icon icon="ion:logo-css3" className="display-4" />,
-    name: "CSS3",
+    skill: <Icon icon="bxl:java" className="display-4" />,
+    name: "Java",
   },
   {
     id: 3,
-    skill: <Icon icon="fa6-brands:js" className="display-4" />,
-    name: "JavaScript",
+    skill: <Icon icon="devicon:sqldeveloper" className="display-4" />,
+    name: "SQL",
   },
   {
     id: 4,
-    skill: <Icon icon="ri:bootstrap-fill" className="display-4" />,
-    name: "BootStrap",
+    skill: <Icon icon="mdi:docker" className="display-4" />,
+    name: "Docker",
   },
   {
     id: 5,
-    skill: <Icon icon="mdi:react" className="display-4" />,
-    name: "React",
+    skill: <Icon icon="devicon-plain:bash" className="display-4" />,
+    name: "Bash",
   },
   {
     id: 6,
-    skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
-    name: "Styled Components",
+    skill: <Icon icon="simple-icons:r" className="display-4" />,
+    name: "R",
   },
   {
     id: 7,
-    skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
-    name: "Redux",
+    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
+    name: "GitHub",
   },
   {
     id: 8,
@@ -80,26 +83,34 @@ export const skillData = [
   },
   {
     id: 9,
-    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
-    name: "GitHub",
+    skill: <Icon icon="devicon-plain:prolog" className="display-4" />,
+    name: "Prolog",
   },
 ];
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
-export const resume = null;
+export const resume = "https://beige-bette-86.tiiny.site";
 
 /* Projects
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["example-1", "example-2", "example-3"];
+export const filteredProjects = ["alienInvasionAI", "Domino", "Market-Analysis-Google-Reviews"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 7-8)
 export const projectCardImages = [
   {
-    name: "example-1",
+    name: "alienInvasionAI",
     image: Logo,
   },
+  {
+    name:"Domino",
+    image:Logo1,
+  },
+  {
+    name:"Market-Analysis-Google-Reviews",
+    image:Logo3,
+  }
 ];
 
 /* Contact Info
@@ -107,4 +118,4 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/xeqbvqjl";
